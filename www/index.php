7e3592c4af9d476f5a4b32da8983dcb93a16daa7
Neville Andrade <neville.andrade@yahoo.com>
2020-01-05 20:43:01 +0100

    Update index.php

--- www/index.php
+++ www/index.php
@@ -5,7 +5,7 @@
 <body>
 <h1>Pi Relay Control App</h1>
 <?php
-$mysqli = new mysqli('localhost','root','ga01r4956','pi');
+$mysqli = new mysqli('localhost','root','xxxxxx','pi');
 if ($mysqli->connect_error) 
 {
  die('Connect Error ('. $mysqli->connect_error . ')' . $mysqli->connect_error);
